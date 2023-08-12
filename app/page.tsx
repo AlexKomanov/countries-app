d3f6dd67ca807b7ee88da0989216c5cf03f253c4
AlexKomanov <akomanov88@gmail.com>
2023-08-12 13:24:13 +0300

    Adding typing and articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 
 type Country = {
   name: {
     common: string;
+  },
+  flags: {
+    svg: string;
+    alt: string;
   }
 }
 async function getCountries(): Promise<Country[]> {
@@ -13,8 +18,24 @@ export default async function Home() {
   return (
       <section className="grid grid-cols-5 w-full container gap-2 mt-16">
         {countries.map((country) => (
-          <h1 key={country.name.common}>{country.name.common}</h1>
-        ))}
+            <article
+                className="h-64 min-w-full p-2 bg-gray-100 border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl "
+                key={country.name.common}
+            >
+              <div className="relative w-full h-40 p-2 overflow-hidden rounded-x1">
+                <Image src={country.flags.svg}
+                       alt={country.flags.alt}
+                       fill
+                       className="object-contain"
+                />
+              </div>
+          <h1 key={country.name.common}
+              className="font-bold text-xl text-center mt-1"
+          >
+            {country.name.common}
+          </h1>
+            </article>
+              ))}
       </section>
   )
 }
