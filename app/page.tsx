e528ec51fbc776f60d9bb49e794b7ce0818fdb91
AlexKomanov <akomanov88@gmail.com>
2023-08-12 12:38:55 +0300

    Adding fetch of countries

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import Image from 'next/image'
 
-export default function Home() {
+type Country = {
+  name: {
+    common: string;
+  }
+}
+async function getCountries(): Promise<Country[]> {
+  const countriesResponse = await fetch("https://restcountries.com/v3.1/all");
+  return countriesResponse.json();
+}
+export default async function Home() {
+  const countries = await getCountries();
   return (
-      <section></section>
+      <section className="grid grid-cols-5 w-full container gap-2 mt-16">
+        {countries.map((country) => (
+          <h1 key={country.name.common}>{country.name.common}</h1>
+        ))}
+      </section>
   )
 }
